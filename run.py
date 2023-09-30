3ac1bb5f067847b16daea576efddf21125a25cd2
Joel Leon Gfeller <joelleon.gfeller@icloud.com>
2023-09-30 00:48:04 +0000

    add deck to run.py

--- run.py
+++ run.py
@@ -1,3 +1,9 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+deck = [2, 3, 4, 5, 6, 7, 8, 9, 10, 2, 3, 4, 5, 6, 7, 8, 9, 10, 2, 3, 4, 5, 6, 7, 8, 9, 10, 2, 3, 4, 5, 6, 7, 8, 9, 10
+'J', 'Q', 'K', 'A', 'J', 'Q', 'K', 'A', 'J', 'Q', 'K', 'A', 'J', 'Q', 'K', 'A']
+playerHand = []
+dealerHand = []
+
+
+
+
+
