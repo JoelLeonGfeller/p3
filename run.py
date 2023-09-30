f3300cf9a351d8cc79e400110b8476eedf70bbb0
Joel Leon Gfeller <joelleon.gfeller@icloud.com>
2023-09-30 00:54:09 +0000

    add dealing cards

--- run.py
+++ run.py
@@ -2,8 +2,11 @@ deck = [2, 3, 4, 5, 6, 7, 8, 9, 10, 2, 3, 4, 5, 6, 7, 8, 9, 10, 2, 3, 4, 5, 6, 7
 'J', 'Q', 'K', 'A', 'J', 'Q', 'K', 'A', 'J', 'Q', 'K', 'A', 'J', 'Q', 'K', 'A']
 playerHand = []
 dealerHand = []
-
-
+ 
+def dealCard(turn):
+    card = random.choice(deck)
+    turn.append(card)
+    deck.remove(card)
 
 
 
