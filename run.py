40049e1854f291d00fb2d4c56ad5b593e4f91368
Joel Leon Gfeller <joelleon.gfeller@icloud.com>
2023-09-30 04:49:17 +0000

    add comments and final touches

--- run.py
+++ run.py
@@ -1,84 +1,87 @@
-import random 
-playerIn = True
-dealerIn = True
+import random
 
+# Define the deck of cards
+deck = [2, 3, 4, 5, 6, 7, 8, 9, 10, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+        'J', 'Q', 'K', 'A', 'J', 'Q', 'K', 'A', 'J', 'Q', 'K', 'A', 'J', 'Q', 'K', 'A']
 
-deck = [2, 3, 4, 5, 6, 7, 8, 9, 10, 2, 3, 4, 5, 6, 7, 8, 9, 10, 2, 3, 4, 5, 6, 7, 8, 9, 10, 2, 3, 4, 5, 6, 7, 8, 9, 10,
-'J', 'Q', 'K', 'A', 'J', 'Q', 'K', 'A', 'J', 'Q', 'K', 'A', 'J', 'Q', 'K', 'A']
+# Initialize player and dealer hands
 playerHand = []
 dealerHand = []
- 
+
+# Function to deal a card
 def dealCard(turn):
     card = random.choice(deck)
     turn.append(card)
     deck.remove(card)
-    
 
+# Function to calculate the total value of a hand
 def total(turn):
-    total = 0
-    face = ['A', 'J', 'K', 'Q']
+    total_value = 0
+    aces = 0  # To keep track of the number of Aces in the hand
     for card in turn:
-        if card in range(1, 11):
-            total += card
-        elif card in face:
-            total += 10 
-        else:
-            if totsal > 11:
-                total += 1
-            else:
-                total += 11
-        return total                  
+        if card in range(2, 11):
+            total_value += card
+        elif card in ['J', 'Q', 'K']:
+            total_value += 10
+        elif card == 'A':
+            aces += 1
+            total_value += 11
+    # Handle Aces to avoid busting
+    while total_value > 21 and aces:
+        total_value -= 10
+        aces -= 1
+    return total_value
 
-
+# Function to reveal the dealer's hand
 def revealDealerHand():
     if len(dealerHand) == 2:
-        return dealerHand[0]
+        return dealerHand[0], 'X'
     elif len(dealerHand) > 2:
-        return dealerHand [0], dealerHand [1]
-            
+        return dealerHand[0], dealerHand[1]
 
-
+# Initial dealing of cards
 for _ in range(2):
     dealCard(dealerHand)
     dealCard(playerHand)
 
-
-while playerIn or dealerIn:
-    print(f"Dealer had {revealDealerHand()}and X") 
+# Game loop
+while True:
+    print(f"Dealer has {revealDealerHand()}")
     print(f"You have {playerHand} for a total of {total(playerHand)}")
-    if playerIn:
-        stayOrHit = input("1: Stay\n2: Hit\n")
-    if total(dealerHand) > 16:
-        dealerIn = False
-    else:
-        dealeCard(dealerHand)
-    if stayOrHit == '1':
-        playerIn = False
-    else:
-        dealCard(playerHand)
+
     if total(playerHand) >= 21:
         break
-    elif total(dealerHand) >= 21:
-        break
 
-if total(playerHand) == 21:
-    print (f"\nYou have {playerHand} for a total of 21 and the dealer has {dealerHand} for a total of {total(dealerHand)}")
-    print("Blackjack! You win!")
-elif total(dealHand) == 21:
-    print(f"\nYou have {playerHand} for a total of 21 and the dealer has {dealerHand} for a total of {total(dealerHand)}")
-    print("Blackjack! Dealer wins!")
-elif total(playerHand) > 21:
-    print(f"\nYou have {playerHand} for a total of 21 and the dealer has {dealerHand} for a total of {total(dealerHand)}")
+    choice = input("1: Stay\n2: Hit\n")
+    
+    if choice == '1':
+        break
+    elif choice == '2':
+        dealCard(playerHand)
+    else:
+        print("Invalid input. Please select 1 or 2.")
+
+# Dealer's turn
+while total(dealerHand) < 17:
+    dealCard(dealerHand)
+
+# Determine the winner
+player_total = total(playerHand)
+dealer_total = total(dealerHand)
+
+print(f"\nYou have {playerHand} for a total of {player_total}")
+print(f"Dealer has {dealerHand} for a total of {dealer_total}")
+
+if player_total > 21:
     print("You bust! Dealer Wins!")
-elif total(dealerHand) > 21:
-    print(f"\nYou have {playerHand} for a total of 21 and the dealer has {dealerHand} for a total of {total(dealerHand)}")  
-    print("DEaler bust! You Win!") 
-elif 21 -total(dealerHand) < 21 - total(playerHand):
-    print(f"\nYou have {playerHand} for a total of 21 and the dealer has {dealerHand} for a total of {total(dealerHand)}")
-    print("Dealer Wins!") 
-elif 21 - total(dealerHand)  > 21 - total(playerHand):
-    print(f"\nYou have {playerHand} for a total of 21 and the dealer has {dealerHand} for a total of {total(dealerHand)}")
-    print("You Win!")      
+elif dealer_total > 21:
+    print("Dealer busts! You Win!")
+elif dealer_total == player_total:
+    print("It's a tie!")
+elif dealer_total > player_total:
+    print("Dealer Wins!")
+else:
+    print("You Win!")
 
 
 
